75e078bf2cca6c52bad37953017b6eccaf86229a
Imran Shah <imranshah10140@gmail.com>
2024-03-26 15:20:55 +0530

    Coin Flip

--- src/pages/apps/Toss.tsx
+++ src/pages/apps/Toss.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import AdminSideBar from "../../components/AdminSideBar";
 
 const Toss = () => {
+  const [angle, setAngle] = useState<number>(0);
+
+  const flipCoin = () => {
+    if (Math.random() > 0.5) setAngle((prev) => prev + 180);
+    else setAngle((prev) => prev + 360);
+  };
+
   return (
     <div className="adminContainer">
       <AdminSideBar />
-      <main className="chart-container">Toss</main>
+      <main className="dashboard-app-container">
+        <h1>Toss</h1>
+        <section>
+          <article
+            className="tosscoin"
+            onClick={flipCoin}
+            style={{ transform: `rotateY(${angle}deg)` }}
+          >
+            <div></div>
+            <div></div>
+          </article>
+        </section>
+      </main>
     </div>
   );
 };
